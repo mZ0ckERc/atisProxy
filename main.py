686b642304923bfcdaeae0656873337f4e72a840
BuildTools <unconfigured@null.spigotmc.org>
2024-02-24 19:33:17 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from typing import Union
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+import requests
+import urllib
+
+appendedText = ""
+
+lastQueryParams = None
+
+overriddenParams: dict = {}
+
+app = FastAPI()
+
+
+
+@app.get("/atis.php", response_class=HTMLResponse)
+async def getMETAR(request: Request):
+    global lastQueryParams
+    lastQueryParams = request.query_params
+    finalParams = dict(lastQueryParams).copy()
+    finalParams.update(overriddenParams)
+    print(f"https://www.uniatis.net/atis.php?{finalParams}")
+    print(requests.get(f"https://www.uniatis.net/atis.php?{urllib.parse.urlencode(finalParams)}").url)
+    uniatisText = requests.get(f"https://www.uniatis.net/atis.php?{urllib.parse.urlencode(finalParams)}").text
+    finalText = uniatisText + appendedText
+    return finalText
+
+# Set different Variables
+
+@app.get("/setAppended")
+async def append(text: str = ""):
+    global appendedText
+    appendedText = text
+    return appendedText
+
+@app.get("/getAppended")
+async def getAppend():
+    return appendedText
+
+@app.get("/getLastQueryParams")
+async def getLastQueryParams():
+    return lastQueryParams
+
+@app.get("/getCurrentParams")
+async def getCurrentParams():
+    if lastQueryParams:
+        z = dict(lastQueryParams).copy()
+        z.update(overriddenParams)
+    else:
+        z = overriddenParams.copy()
+    return z
+
+@app.get("/test")
+async def text(request: Request):
+    return ""
